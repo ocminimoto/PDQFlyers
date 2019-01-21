4a01b138b94f6d25762ea953225c94f4fd36dd30
Michael Gaskin <gaskin.cet@gmail.com>
2019-01-21 13:03:05 -0800

    second commit fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,31 @@
 
-  	var navbar = document.getElementById('navbar')
+  	var nav = document.getElementById('navbar');
 
   	  window.onscroll = function () {
   	  	 
-  	  	 if (window.pageYOffset > 100) {
+  	  	 if(window.pageYOffset > 100) {
 
   	  	 	nav.style.background = "#007bff";
   	  	 	nav.style.boxShadow = "0px 4px 2px blue";
+          nav.style.position = "fixed";
+          nav.style.top = 0;
   	  	  }
   	  	    else
             {
   	  	      nav.style.background = "transparent";	
   	  	      nav.style.boxShadow = "none";
   	  	      nav.style.top =100;
+              nav.style.position = "absolute";
+              nav.style.top =100;
   	  	    }
   	  	 }	
 
-         function myFunction_home() {
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
+       function myFunction_home() {
             location.href = "index.html";
         }  
-
-        var nav = document.getElementById('navbar')
-
-      window.onscroll = function () {
-         
-         if (window.pageYOffset > 100) {
-
-          nav.style.position = "fixed";
-          nav.style.top = 0;
-
-            }else{
-              nav.style.position = "absolute";
-              nav.style.top =100;
-            }
-         }
